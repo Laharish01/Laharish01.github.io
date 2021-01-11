aed77084f6426e27a447b16a7748eda36f6c7199
Laharish01 <laharish2k17@gmail.com>
2021-01-11 20:36:08 +0530

    fixed dark mode bug

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
-var btnToggle = document.querySelector(".dark-mode-toggle");
-
-if(window.matchMedia('preferes-color-scheme: dark')){
-    btnToggle.innerHTML = "Dark"
-}
-
+var btnToggle = document.querySelector(".slider");
 btnToggle.addEventListener('click', function() {
     document.body.classList.toggle("dark");
-    if (document.body.classList.contains('dark')){
-        btnToggle.innerHTML = "Light"
-    }else {
-        btnToggle.innerHTML = "Dark"
-
-    }
 })
+
